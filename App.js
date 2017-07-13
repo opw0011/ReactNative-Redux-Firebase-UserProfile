d144a655d06524f7aceaebc7f78388cc8a296a5c
Benny O <opw0011@gmail.com>
2017-07-13 20:49:32 +0800

    Code clean up

--- App.js
+++ App.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux'; 
-
-// import * as firebase from 'firebase';
-// import { firebaseConfig } from './src/configs/config';
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from './src/reducers';
-
 import Profile from './src/containers/Profile';
 
 const store = createStore(
